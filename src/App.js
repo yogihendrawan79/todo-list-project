f8c675478f2f117d668291e19778f9eff0599010
Yogi Hendrawan <hendrawanyogi79@gmail.com>
2022-07-22 13:13:57 +0700

    feat: setup tailwind and daisyui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Navbar from "./components/Navbar";
+import ToDoItem from "./components/ToDoItem";
+
+const App = () => {
+  const [task, setTask] = useState("");
+  const [taskLists, setTaskLists] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskLists([...taskLists, task]);
+    setTask("");
+  };
+
+  const doneTask = (index) => {
+    let tempTaskLists = [...taskLists];
+    tempTaskLists.splice(index, 1);
+    setTaskLists(tempTaskLists);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="flex place-content-center">
+        <div className="card w-96 bg-base-100 shadow-xl mb-10">
+          <div className="card-body">
+            <h2 className="card-title">Insert Your ToDo-List</h2>
+            <div className="form-control">
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Input Your ToDo-List Here..."
+                  className="input input-bordered w-full"
+                  onChange={(text) => setTask(text.target.value)}
+                  value={task}
+                />
+                <button
+                  className="btn btn-square bg-blue-300"
+                  onClick={() => handleAddTask()}
+                >
+                  ADD
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        {taskLists.length > 0 ? (
+          taskLists.map((task, index) => {
+            return (
+              <ToDoItem
+                key={index}
+                task={task}
+                onClick={() => doneTask(index)}
+              />
+            );
+          })
+        ) : (
+          <div className="flex place-content-center">
+            <p>
+              <i>You haven't create todo list yet.</i>
+            </p>
+          </div>
+        )}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
